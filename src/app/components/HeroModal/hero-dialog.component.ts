fc9ff04a6fe04964684140d3e7dbc920ce63566c
Bennett Robinson <brobinson9895@gmail.com>
2025-08-06 14:05:18 -0600

    add dialog modal for crud heroes

---
+++ src/app/components/HeroModal/hero-dialog.component.ts
@@ -0,0 +1,57 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-create-dialog',
+  standalone: true,
+  templateUrl: './hero-dialog.component.html',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ]
+})
+export class HeroCreateDialogComponent {
+  heroForm: FormGroup;
+  dialogRef = inject(MatDialogRef<HeroCreateDialogComponent>);
+  data: any = inject(MAT_DIALOG_DATA)
+  fb = inject(FormBuilder);
+  isCreate: boolean = false
+
+  constructor() {
+    this.isCreate = this.data.crud === 'create'
+    this.heroForm = this.fb.group({
+      id: [+this.data?.id],
+      crud: [this.data?.crud],
+      name: [this.data?.name, Validators.required],
+      url: [this.data?.url],
+      win_rate: [this.data?.['win_rate'], [Validators.required, Validators.min(0), Validators.max(100)]],
+      games_played: [this.data?.['total_talishar_plays'], [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.heroForm.valid) {
+      this.dialogRef.close(this.heroForm.value); // Pass form data back
+    }
+  }
+
+  onDelete() {
+    if(this.heroForm.value.id){
+      this.heroForm.patchValue({crud: 'delete'})
+      this.dialogRef.close(this.heroForm.value)
+    }
+  }
+
+  onCancel() {
+    this.dialogRef.close(); // Simply close the dialog
+  }
+}

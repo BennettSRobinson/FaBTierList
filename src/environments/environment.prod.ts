416a747cff94a02d902ef96dc097daea2d6eca09
Bennett Robinson <brobinson9895@gmail.com>
2025-08-11 13:51:04 -0600

    add prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend.onrender.com/graphql' // Your Render backend URL
+  apiUrl: 'https://fabtierlist-server.onrender.com/graphql' // Your Render backend URL
 };
